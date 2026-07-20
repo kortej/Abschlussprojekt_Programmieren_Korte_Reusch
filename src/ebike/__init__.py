73abb0001321d256086f0b9bedf262d8522e9eaa
kortej <jo.korte@mci4me.at>
2026-07-20 18:57:44 +0200

    feat(ebike): Package-Init mit öffentlichen Exporten

---
+++ src/ebike/__init__.py
@@ -0,0 +1,25 @@
+"""E-Bike-Simulation - Abschlussprojekt Programmieren 1 (MCI).
+
+Das Paket berechnet aus aufgezeichneten GPS-Daten die Fahrwiderstaende eines
+E-Bikes und simuliert damit den Ladezustand zweier Akkutypen.
+"""
+
+__version__ = "1.1.0"
+
+from .bike import EBike
+from .battery import BatteryPack, LiPoAkku, NmcAkku, erzeuge_akku
+from .config import ProjectConfig
+from .data_loader import Track
+from .simulation import FahrtSimulator, SimulationsErgebnis
+
+__all__ = [
+    "EBike",
+    "BatteryPack",
+    "LiPoAkku",
+    "NmcAkku",
+    "erzeuge_akku",
+    "ProjectConfig",
+    "Track",
+    "FahrtSimulator",
+    "SimulationsErgebnis",
+]
